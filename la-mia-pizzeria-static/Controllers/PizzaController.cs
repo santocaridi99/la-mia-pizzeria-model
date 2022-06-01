6947b0e863b046e3c7d204b8825283b2fd67a17b
santocaridi99 <santocaridi99@gmail.com>
2022-06-01 17:27:08 +0200

    Importa Dati in modo Dinamico

---
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -0,0 +1,25 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            Pizza margherita = new Pizza("Pizza Margherita", "pomodoro , mozzarella campana , basilico", "./img/pizza-margherita-2-6yehdnu31vrv1puavcja7g753ipcgihq8vyh1ifv5pw.jpg",3);
+            Pizza capricciosa = new Pizza("Pizza Capricciosa", "Che soddisfa ogni capriccio", "./img/salsicciaepatate.jpg", 4);
+            Pizza salsicciaPatate = new Pizza("Pizza Salsiccia e Patate", "salsiccia , patate , mozzarella capana", "./img/salsicciaepatate.jpg", 5);
+            Pizza marinara = new Pizza("Pizza Marinara", "Grande classico", "./img/marinara.jpg", 3);
+            Pizza quattroStagioni = new Pizza("Pizza Quattro Stagioni", "La quattro Stagioni", "./img/Pizza_Quattro_Stagioni_transparent.png", 4);
+            listaPizze pizze = new listaPizze();
+            pizze.listaDiPizze.Add(margherita);
+            pizze.listaDiPizze.Add(capricciosa);
+            pizze.listaDiPizze.Add(salsicciaPatate);
+            pizze.listaDiPizze.Add(marinara);
+            pizze.listaDiPizze.Add(quattroStagioni);
+            return View(pizze);
+        }
+    }
+}
---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,22 @@
+﻿
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+
+        public string Foto { get; set; }
+
+        public double Prezzo { get; set; }
+
+        public Pizza(string nome, string descrizione, string foto, double prezzo)
+        {
+            Nome = nome;
+            Descrizione = descrizione; 
+            Foto = foto;
+            Prezzo = prezzo;
+           
+        }
+    }
+}
